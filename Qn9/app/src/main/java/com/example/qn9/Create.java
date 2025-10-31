ecb14ab6f88d2bc6b62917231bd06e08c9e88349
georgeksaji <georgeksaji14@gmail.com>
2025-10-31 07:48:51 +0530

    completed insertion

--- Qn9/app/src/main/java/com/example/qn9/Create.java
+++ Qn9/app/src/main/java/com/example/qn9/Create.java
@@ -10,12 +10,36 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Create extends AppCompatActivity {
-
+    Dbhelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.create);
+
+        db = new Dbhelper(this);
+        Button btn = findViewById(R.id.button3);
+        EditText et1 = findViewById(R.id.editTextNumber);
+        EditText et2 = findViewById(R.id.editTextText4);
+
+
+        btn.setOnClickListener(v->{
+            int id = Integer.parseInt(et1.getText().toString().trim());
+            String name = et2.getText().toString();
+
+            if(id <= 0 || name.isEmpty())
+            {
+                Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(db.insertStudent(id,name))
+            {
+                Toast.makeText(this, "Inserted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Failed Insertion", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
---
+++ Qn9/app/src/main/java/com/example/qn9/Dbhelper.java
@@ -0,0 +1,56 @@
+package com.example.qn9;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Dbhelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "Studentdb";
+    private static final int DB_VERSION = 1;
+
+    private static final String TABLE_NAME = "Student";
+    private static final String COL_ID  = "ID";
+    private static final String COL_NAME  = "Name";
+
+    public Dbhelper(Context context)
+    {
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+TABLE_NAME+" ( "+COL_ID+" INTEGER PRIMARY KEY,"+COL_NAME+" TEXT NOT NULL)");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertStudent(int id, String name)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ID",id);
+        cv.put("Name",name);
+        long result = db.insert("Student",null,cv);
+        db.close();
+        if(result!=-1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateStudent(int id, String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("Name", name);
+        int result = db.update("Student", cv, "ID = ?", new String[]{String.valueOf(id)});
+        db.close();
+        return result > 0;
+    }
+}
